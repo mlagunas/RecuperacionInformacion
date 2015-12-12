15d58400562768d6162a8666a1c29020a1734894
daniSalva <dsalv57@gmail.com>
2015-12-12 12:27:42 +0100

    a

--- RecuperacionPracticas/src/practica3/Evaluation.java
+++ RecuperacionPracticas/src/practica3/Evaluation.java
@@ -25,10 +25,7 @@ public class Evaluation {
 
 	public static void main(String[] args) {
 		readFiles();
-		System.out.println(precision(1, qrelsR, resultRead));
-
 	}
-
 	public static double precision(int need,
 			HashMap<String, ArrayList<String>> rel,
 			HashMap<String, ArrayList<String>> recu) {
