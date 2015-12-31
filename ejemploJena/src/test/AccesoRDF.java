a922b5e9091506c3f25f20040028784ff9c3b225
daniSalva <dsalv57@gmail.com>
2015-12-31 01:09:43 +0100

    p5

---
+++ ejemploJena/src/test/AccesoRDF.java
@@ -0,0 +1,84 @@
+package test;
+
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.Property;
+import com.hp.hpl.jena.rdf.model.RDFNode;
+import com.hp.hpl.jena.rdf.model.ResIterator;
+import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.rdf.model.Statement;
+import com.hp.hpl.jena.rdf.model.StmtIterator;
+import com.hp.hpl.jena.util.FileManager;
+
+public class AccesoRDF {
+
+	/**
+	 * accede de diferentes maneras a las propiedades de un modelo rdf
+	 */
+	public static void main(String args[]) {
+
+		// cargamos el fichero deseado
+		Model model = FileManager.get().loadModel("card.rdf");
+
+		// obtenemos todos los statements del modelo
+		StmtIterator it = model.listStatements();
+
+		// mostramos todas las tripletas cuyo objeto es un literal
+		while (it.hasNext()) {
+			Statement st = it.next();
+
+			if (st.getObject().isLiteral()) {
+				System.out.println(st.getSubject().getURI() + " - "
+						+ st.getPredicate().getURI() + " - "
+						+ st.getLiteral().toString());
+			}
+		}
+
+		System.out.println("----------------------------------------");
+
+		// mostramos los valores de todas las propiedades de un recurso
+		// determinado
+		Resource res = model
+				.getResource("http://dig.csail.mit.edu/2008/webdav/timbl/foaf.rdf");
+		it = res.listProperties();
+		while (it.hasNext()) {
+			Statement st = it.next();
+
+			if (st.getObject().isLiteral()) {
+				System.out.println(st.getSubject().getURI() + " - "
+						+ st.getPredicate().getURI() + " - "
+						+ st.getLiteral().toString());
+			} else {
+				System.out.println(st.getSubject().getURI() + " - "
+						+ st.getPredicate().getURI() + " - "
+						+ st.getResource().getURI());
+			}
+		}
+
+		System.out.println("----------------------------------------");
+
+		// mostramos todos los recursos que contienen una propiedad determinada
+		Property prop = model
+				.getProperty("http://purl.org/dc/elements/1.1/title");
+		ResIterator ri = model.listSubjectsWithProperty(prop);
+		while (ri.hasNext()) {
+			Resource r = ri.next();
+			System.out.println(r.getURI());
+		}
+
+		System.out.println("----------------------------------------");
+
+		// mostramos todos los recursos que contienen una propiedad determinada
+		// forma alternativa que usa un filtro sobre los statements a recuperar
+		it = model.listStatements(null, prop, (RDFNode) null);
+		while (it.hasNext()) {
+			Statement st = it.next();
+
+			if (st.getObject().isLiteral()) {
+				System.out.println(st.getSubject().getURI() + " - "
+						+ st.getPredicate().getURI() + " - "
+						+ st.getLiteral().toString());
+			}
+		}
+	}
+
+}
