803e2db07a7446ebd9e3501e45f171413ebefdc1
Manuel <mlagunas94@gmail.com>
2016-01-05 13:33:03 +0100

    Uso librerias para simplificacion

--- Jena/src/test/CreacionRDF_Trabajo.java
+++ Jena/src/test/CreacionRDF_Trabajo.java
@@ -23,93 +23,111 @@ import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
 import com.hp.hpl.jena.rdf.model.Property;
 import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.sparql.vocabulary.FOAF;
+import com.hp.hpl.jena.vocabulary.DC;
 import com.hp.hpl.jena.vocabulary.RDF;
 import com.hp.hpl.jena.vocabulary.VCARD;
 
 public class CreacionRDF_Trabajo {
-	
+
 	/**
 	 * muestra un modelo de jena de ejemplo por pantalla
 	 */
-	public static void main (String args[]) {
-        Model model = CreacionRDF_Trabajo.generarEjemplo();
-        // write the model in the standar output
-        model.write(System.out); 
-    }
-	
+	public static void main(String args[]) {
+		Model model = CreacionRDF_Trabajo.generarEjemplo();
+		// write the model in the standar output
+		model.write(System.out);
+	}
+
 	/**
 	 * Genera un modelo de jena de ejemplo
 	 */
-	public static Model generarEjemplo(){
+	public static Model generarEjemplo() {
 		// definiciones
-        String trabajoURI    = "https://zaguan.unizar.es/record/32726";
-        String t    = "Web 2.0 y relaciones sociales";
-        String author   = "Vázquez Toledo, Sandra";
-        String ty= "TFG";
+		String trabajoURI = "https://zaguan.unizar.es/record/32726";
+		String t = "Web 2.0 y relaciones sociales";
+		String author = "Vázquez Toledo, Sandra";
+		String ty = "TFG";
 
-        // crea un modelo vacio
-        Model model = ModelFactory.createDefaultModel();
+		// crea un modelo vacio
+		Model model = ModelFactory.createDefaultModel();
 
-        Property type = model.createProperty("http://www.w3.org/1999/02/22-rdf-syntax-ns#type");
-        Resource person = model.createResource("http://xmlns.com/foaf/0.1/person");
-        Property knows = model.createProperty("http://xmlns.com/foaf/0.1/knows");
- 
-        Property title= model.createProperty("http://purl.org/dc/elements/1.1/title");
-        Property creator= model.createProperty("http://purl.org/dc/elements/1.1/creator");
-        Property identifier= model.createProperty("http://purl.org/dc/elements/1.1/identifier");
-        Property subject= model.createProperty("http://purl.org/dc/elements/1.1/subject");
-        Property typeOf= model.createProperty("http://purl.org/dc/elements/1.1/type");
-        Property description= model.createProperty("http://purl.org/dc/elements/1.1/description");
-        Property publisher= model.createProperty("http://purl.org/dc/elements/1.1/publisher");
-        Property format= model.createProperty("http://purl.org/dc/elements/1.1/format");
-        Property language= model.createProperty("http://purl.org/dc/elements/1.1/language");
-        
-        String skos = "http://www.w3.org/2004/02/skos/core#";
-        model.setNsPrefix( "skos", skos );
+		//
+		// USAR esto? RDF.type, FOAF.Person, DC.title;
+		//
 
-        Resource skosConcept = model.createResource(skos+"Concept" );
-        Resource conceptScheme = model.createResource(skos+ "ConceptScheme");
-        Property prefLabel = model.createProperty( skos + "prefLabel");
-    	Property altLabel = model.createProperty( skos + "altLabel");
-    	Property broader = model.createProperty( skos + "broader"); //PERTENECE A (SUBCLASE DE)
-    	Property narrower = model.createProperty( skos + "narrower"); //SUPERCLASE  DE
-    	Property related = model.createProperty( skos + "related");
-    	Property inScheme = model.createProperty( skos + "inScheme");
-    	Property definition = model.createProperty( skos + "definition");
+		/*Property type = model
+				.createProperty("http://www.w3.org/1999/02/22-rdf-syntax-ns#type");
+		Resource person = model
+				.createResource("http://xmlns.com/foaf/0.1/person");
+		Property knows = model
+				.createProperty("http://xmlns.com/foaf/0.1/knows");
+		Property title = model
+				.createProperty("http://purl.org/dc/elements/1.1/title");
+		Property creator = model
+				.createProperty("http://purl.org/dc/elements/1.1/creator");
+		Property identifier = model
+				.createProperty("http://purl.org/dc/elements/1.1/identifier");
+		Property subject = model
+				.createProperty("http://purl.org/dc/elements/1.1/subject");
+		Property typeOf = model
+				.createProperty("http://purl.org/dc/elements/1.1/type");
+		Property description = model
+				.createProperty("http://purl.org/dc/elements/1.1/description");
+		Property publisher = model
+				.createProperty("http://purl.org/dc/elements/1.1/publisher");
+		Property format = model
+				.createProperty("http://purl.org/dc/elements/1.1/format");
+		Property language = model
+				.createProperty("http://purl.org/dc/elements/1.1/language");*/
 
-        Resource scheme=model.createResource();
-        model.add(scheme,type,conceptScheme);
-        model.add(scheme,title,"Tipos de trabajos");
-        
-        Resource trabajo = model.createResource().addProperty(type, skosConcept)
-        		.addProperty(inScheme, scheme)
-        		.addProperty(prefLabel, "Trabajo");
-        
-        Resource TFG = model.createResource().addProperty(type, skosConcept)
-        		.addProperty(inScheme, scheme)
-        		.addProperty(prefLabel, "TFG")
-        		.addProperty(altLabel, "Trabajo Fin de Grado")
-        		.addProperty(broader, trabajo);
-        
-        Resource TFM = model.createResource().addProperty(type, skosConcept)
-        		.addProperty(inScheme, scheme)
-        		.addProperty(prefLabel, "TFM")
-        		.addProperty(altLabel, "Trabajo Fin de Master")
-        		.addProperty(broader, trabajo);
-        
-        Resource PFC = model.createResource().addProperty(type, skosConcept)
-        		.addProperty(inScheme, scheme)
-        		.addProperty(prefLabel, "PFC")
-        		.addProperty(altLabel, "Proyecto Fin de Carrera")
-        		.addProperty(broader, trabajo);
-        
-        Resource t1=model.createResource(trabajoURI).addProperty(title, t)
-        		.addProperty(creator, author).addProperty(typeOf, ty);
-        
-        model.add(t1,type,TFG);
-        
-        return model;
+		//http://www.w3.org/2009/08/skos-reference/skos.html
+		String skos = "http://www.w3.org/2004/02/skos/core#";
+		model.setNsPrefix("skos", skos);
+
+		Resource skosConcept = model.createResource(skos + "Concept");
+		Resource conceptScheme = model.createResource(skos + "ConceptScheme");
+		Property prefLabel = model.createProperty(skos + "prefLabel");
+		Property altLabel = model.createProperty(skos + "altLabel");
+		Property broader = model.createProperty(skos + "broader"); // PERTENECE
+																	// A
+																	// (SUBCLASE
+																	// DE)
+		Property narrower = model.createProperty(skos + "narrower"); // SUPERCLASE
+																		// DE
+		Property related = model.createProperty(skos + "related");
+		Property inScheme = model.createProperty(skos + "inScheme");
+		Property definition = model.createProperty(skos + "definition");
+
+		Resource scheme = model.createResource();
+		model.add(scheme, RDF.type, conceptScheme);
+		model.add(scheme, DC.title, "Tipos de trabajos");
+
+		Resource trabajo = model.createResource()
+				.addProperty(RDF.type, skosConcept).addProperty(inScheme, scheme)
+				.addProperty(prefLabel, "Trabajo");
+
+		Resource TFG = model.createResource().addProperty(RDF.type, skosConcept)
+				.addProperty(inScheme, scheme).addProperty(prefLabel, "TFG")
+				.addProperty(altLabel, "Trabajo Fin de Grado")
+				.addProperty(broader, trabajo);
+
+		Resource TFM = model.createResource().addProperty(RDF.type, skosConcept)
+				.addProperty(inScheme, scheme).addProperty(prefLabel, "TFM")
+				.addProperty(altLabel, "Trabajo Fin de Master")
+				.addProperty(broader, trabajo);
+
+		Resource PFC = model.createResource().addProperty(RDF.type, skosConcept)
+				.addProperty(inScheme, scheme).addProperty(prefLabel, "PFC")
+				.addProperty(altLabel, "Proyecto Fin de Carrera")
+				.addProperty(broader, trabajo);
+
+		Resource t1 = model.createResource(trabajoURI).addProperty(DC.title, t)
+				.addProperty(DC.creator, author).addProperty(DC.type, ty);
+
+		model.add(t1, RDF.type, TFG);
+
+		return model;
 	}
-	
-	
+
 }
