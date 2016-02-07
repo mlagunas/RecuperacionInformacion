fa22ffda57825dcd099feea0a404812c11d0f8cb
Daniel Salvador <dsalv57@gmail.com>
2016-02-07 12:36:16 +0100

    sparql ok

--- Jena/src/test/SPARQLTrabajo.java
+++ Jena/src/test/SPARQLTrabajo.java
@@ -28,53 +28,31 @@ public class SPARQLTrabajo {
 		Model skos = FileManager.get().loadModel("trabajo_skos.rdf");
 		model.add(skos);
 		
+		System.out.println("----------------------------------------");		
+		System.out.println("--------------CONSULTA 1----------------");
 		System.out.println("----------------------------------------");
 
-		 String queryString = 
-				 	  
-				 	"PREFIX dmrec: <http://www.recInfo.org/dm/> "
+		 String queryString = "PREFIX dmrec: <http://www.recInfo.org/dm/> "
 		 			+ "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> "
 		 			+ "PREFIX skos: <http://www.w3.org/2004/02/skos/core#>  "
 		 			+ "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>  "
-			 		
-		 			+ "SELECT ?narrower WHERE {  "
-			 		+ "?skosQ1 rdf:type skos:concept ."
-			 		+ "?skosQ1 skos:prefLabel ?label . " 		
-			 		+ "?skosQ1 skos:narrower ?narrower ."
-			 		+ "?narrower skos:prefLabel ?prefLabel"
-			 		+ ""
-			 		+ ""
-			 		//+ "?narrower skos:prefLabel ?prefLabel "
-			 		//+ "FILTER regex (?prefLabel,'cancion','i')  "
-			 		+ " }";
-		 String aqueryString = "PREFIX dmrec: <http://www.recInfo.org/dm/> "
-		 			+ "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> "
-		 			+ "PREFIX skos: <http://www.w3.org/2004/02/skos/core#>  "
-		 			+ "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>  "
-			 		+ "SELECT ?label WHERE {  "
+			 		+ "SELECT DISTINCT ?doc WHERE {  "
 			 		+ "?doc rdf:type ?type . "
 			 		+ "?type rdfs:subClassOf dmrec:document . "
 			 		+ "?doc dmrec:creator ?autor . "
 			 		+ "?autor dmrec:name ?name FILTER regex (?name, 'javier','i')  "
-//			 		+ "?skosQ1 rdf:type skos:concept . "
-//			 		+ "?skosQ1 skos:prefLabel ?prefLabel FILTER regex (?prefLabel,'musica','i')  "
-			 		+ "?doc dmrec:keyword ?key . "
-			 		+ "?key skos:narrower+/skos:prefLabel ?label "
-			 		+ "FILTER regex (?label,'musica@sp') "
-
-//			 		+ "?skosQ1 skos:narrower+/skos:prefLabel ?labels1 . "
-//			 		+ "?doc dmrec:keyword ?key . "
-//			 		+ "?key skos:prefLabel ?keyLabel . "
-//					+ "FILTER (str(?keyLabel) = str(?labels1))"
-//			 		+ "?skosQ2 skos:prefLabel ?prefLabel FILTER regex (?prefLabel,'sonido','i')  "
-//			 		+ "?skosQ2 skos:narrower+/skos:prefLabel ?labels2 . "
-//			 		+ "?doc dmrec:keyword ?keyword . "
-//			 		+ "?keyword skos:prefLabel ?keyLabel  "
-//			 		+ "FILTER ( lcase(str(?keyLabel)) = lcase(str(?labels1)) || "
-//			 		+ " lcase(str(?keyLabel)) = lcase(str(?labels2)) ) "
+			 		+ "?skosQ1 rdf:type skos:Concept . "
+			 		+ "?skosQ1 skos:prefLabel ?prefLabel FILTER regex (?prefLabel,'musica','i')  "
+			 		+ "?skosQ1 skos:narrower+/skos:prefLabel ?labels1 . "
+			 		+ "?skosQ2 rdf:type skos:Concept . "
+			 		+ "?skosQ2 skos:prefLabel ?prefLabel2 FILTER regex (?prefLabel2,'sonido','i')  "
+			 		+ "?skosQ2 skos:narrower+/skos:prefLabel ?labels2 . "
+			 		+ "?doc dmrec:keyword ?keyword . "
+			 		+ "?keyword skos:prefLabel ?keyLabel  "
+			 		+ "FILTER ( (str(?keyLabel) = str(?labels1)) || "
+			 		+ " (str(?keyLabel) = str(?labels2)) ) "
 			 		+ " }";
 		 
-		//ejecutamos la consulta y obtenemos los resultados
 		   Query query = QueryFactory.create(queryString) ;
 		   QueryExecution qexec = QueryExecutionFactory.create(query, model) ;
 		  try {
@@ -82,67 +60,150 @@ public class SPARQLTrabajo {
 		    for ( ; results.hasNext() ; )
 		    {
 		    	QuerySolution soln = results.nextSolution() ;
-		    	Literal z = soln.getLiteral("narrower"); 
-		    	System.out.println(z.toString());
+		    	Resource z = soln.getResource("doc"); 
+		    	//System.out.println(z.toString());
+		    }
+		    System.out.println("RESULTS = "+results.getRowNumber());
+		  } finally { qexec.close() ; }
+		  
+		System.out.println("----------------------------------------");		
+		System.out.println("--------------CONSULTA 2----------------");
+		System.out.println("----------------------------------------");
+
+		 queryString = "PREFIX dmrec: <http://www.recInfo.org/dm/>"
+		 			+ "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>"
+		 			+ "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>"
+		 			+ "PREFIX skos: <http://www.w3.org/2004/02/skos/core#>  "
+			 		+ " SELECT DISTINCT ?doc WHERE { "
+			 		+ "?doc rdf:type dmrec:TESIS . "
+			 		+ "?doc dmrec:date ?date FILTER((?date > '2010'^^xsd:gYear) && (?date < '2016'^^xsd:gYear)) "
+			 		+ "?skosQ1 rdf:type skos:Concept . "
+			 		+ "?skosQ1 skos:prefLabel ?prefLabel FILTER regex (?prefLabel,'energias renovables','i') "
+			 		+ "?skosQ1 skos:narrower+/skos:prefLabel ?labels1 . "
+			 		+ "?doc dmrec:keyword ?keyword . "
+			 		+ "?keyword skos:prefLabel ?keyLabel "
+			 		+ "FILTER ( str(?keyLabel) = str(?labels1) ) "
+			 		+ "}";
+		 
+		   query = QueryFactory.create(queryString) ;
+		   qexec = QueryExecutionFactory.create(query, model) ;
+		  try {
+		    ResultSet results = qexec.execSelect() ;
+		    for ( ; results.hasNext() ; )
+		    {
+		    	QuerySolution soln = results.nextSolution() ;
+		    	Resource z = soln.getResource("doc"); 
+		    	//System.out.println(z.toString());
 		    }
 		    System.out.println("RESULTS = "+results.getRowNumber());
 		  } finally { qexec.close() ; }
 
-		 queryString = "PREFIX dmrec: <http://www.recInfo.org/dm/>"
-		 			+ "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>"
-			 		+ " SELECT ?doc WHERE { "
-			 		+ "?doc rdf:type dmrec:TESIS ."
-			 		+ "?doc dmrec:date ?date.FILTER((?date > 2010) && (?date < 2015))"
-			 		+ "?skosQ1 rdf:type skos:concept . "
-			 		+ "?skosQ1 skos:prefLabel ?prefLabel.FILTER regex(?prefLabel,'energias renovables')"
-			 		+ "?skosQ1 skos:narrower+/skos:prefLabel ?labels1"
-			 		+ "?doc dmrec:keyword ?key"
-			 		+ "?keyword skos:prefLabel ?keyLabel"
-			 		+ "FILTER regex(lcase(str(?keyLabel)) = lcase(str(?labels1)))"
+		
+		System.out.println("----------------------------------------");		
+		System.out.println("--------------CONSULTA 3----------------");
+		System.out.println("----------------------------------------");
+		 
+		 queryString = "PREFIX dmrec: <http://www.recInfo.org/dm/> "
+		 			+ "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> "
+		 			+ "PREFIX skos: <http://www.w3.org/2004/02/skos/core#>  "
+		 			+ "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>"
+		 			+ "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>  "
+			 		+ " SELECT DISTINCT ?doc WHERE { "
+			 		+ "?type rdfs:subClassOf dmrec:document . "
+			 		+ "?doc dmrec:date ?date.FILTER (?date > '2010'^^xsd:gYear) "
+			 		+ "?skosQ1 rdf:type skos:Concept . "
+			 		+ "?skosQ1 skos:prefLabel ?prefLabel FILTER regex(?prefLabel,'videojuegos')"
+			 		+ "?skosQ1 skos:narrower+/skos:prefLabel ?labels1 . "
+			 		+ "?skosQ2 rdf:type skos:Concept . "
+			 		+ "?skosQ2 skos:prefLabel ?prefLabel2 FILTER regex(?prefLabel2,'diseño de personajes')"
+			 		+ "?skosQ2 skos:narrower+/skos:prefLabel ?labels2 . "
+			 		+ "?doc dmrec:keyword ?keyword . "
+			 		+ "?keyword skos:prefLabel ?keyLabel  "
+			 		+ "FILTER ( (str(?keyLabel) = str(?labels1)) || "
+			 		+ "(str(?keyLabel) = str(?labels2)) )"
 			 		+ "}";
 		 
-		 queryString = "PREFIX dmrec: <http://www.recInfo.org/dm/>"
-			 		+ " SELECT ?doc WHERE { "
-			 		+ "?doc rdf:type dmrec:documento"
-			 		+ "?doc dmrec:date ?date.FILTER(?date > 2010)"
-			 		+ "?skosQ1 rdf:type skos:concept . "
-			 		+ "?skosQ1 skos:prefLabel ?prefLabel.FILTER regex(?prefLabel,'videojuegos')"
-			 		+ "?skosQ1 skos:narrower+/skos:prefLabel ?labels1"
-			 		+ "?skosQ2 rdf:type skos:concept . "
-			 		+ "?skosQ2 skos:prefLabel ?prefLabel.FILTER regex(?prefLabel,'diseño de personajes')"
-			 		+ "?skosQ2 skos:narrower+/skos:prefLabel ?labels2"
-			 		+ "?doc dmrec:keyword ?keyword"
-			 		+ "?keyword skos:prefLabel ?keyLabel"
-			 		+ "FILTER regex(lcase(str(?keyLabel)) = lcase(str(?labels1)) || "
-			 		+ "lcase(str(?keyLabel)) = lcase(str(?keyLabel)))"
+		  query = QueryFactory.create(queryString) ;
+		   qexec = QueryExecutionFactory.create(query, model) ;
+		  try {
+		    ResultSet results = qexec.execSelect() ;
+		    for ( ; results.hasNext() ; )
+		    {
+		    	QuerySolution soln = results.nextSolution() ;
+		    	Resource z = soln.getResource("doc"); 
+		    	//System.out.println(z.toString());
+		    }
+		    System.out.println("RESULTS = "+results.getRowNumber());
+		  } finally { qexec.close() ; }
+		 
+		  System.out.println("----------------------------------------");		
+		  System.out.println("--------------CONSULTA 4----------------");
+		  System.out.println("----------------------------------------");
+			 
+		 queryString = "PREFIX dmrec: <http://www.recInfo.org/dm/> "
+		 			+ "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> "
+		 			+ "PREFIX skos: <http://www.w3.org/2004/02/skos/core#>  "
+		 			+ "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>  "
+			 		+ " SELECT DISTINCT ?doc WHERE { "
+			 		+ "?doc rdf:type ?type . "
+			 		+ "?type rdfs:subClassOf dmrec:document . "
+			 		+ "?skosQ1 rdf:type skos:Concept . "
+			 		+ "?skosQ1 skos:prefLabel ?prefLabel FILTER regex(?prefLabel,'Guerra de la Independencia','i')"
+			 		+ "?skosQ1 skos:narrower+/skos:prefLabel ?labels1 . "
+			 		+ "?doc dmrec:keyword ?keyword . "
+			 		+ "?keyword skos:prefLabel ?keyLabel "
+			 		+ "FILTER (str(?keyLabel) = str(?labels1))"
 			 		+ "}";
 		 
-		 queryString = "PREFIX dmrec: <http://www.recInfo.org/dm/>"
-			 		+ " SELECT ?doc WHERE { "
-			 		+ "?doc rdf:type dmrec:documento"
-			 		+ "?skosQ1 rdf:type skos:concept . "
-			 		+ "?skosQ1 skos:prefLabel ?prefLabel.FILTER regex(?prefLabel,'Guerra de la Independencia')"
-			 		+ "?skosQ1 skos:narrower+/skos:prefLabel ?labels1"
-			 		+ "?doc dmrec:keyword ?key"
-			 		+ "?keyword skos:prefLabel ?keyLabel"
-			 		+ "FILTER regex(lcase(str(?keyLabel)) = lcase(str(?labels1)))"
-			 		+ "}";
+		  query = QueryFactory.create(queryString) ;
+		   qexec = QueryExecutionFactory.create(query, model) ;
+		  try {
+		    ResultSet results = qexec.execSelect() ;
+		    for ( ; results.hasNext() ; )
+		    {
+		    	QuerySolution soln = results.nextSolution() ;
+		    	Resource z = soln.getResource("doc"); 
+		    	//System.out.println(z.toString());
+		    }
+		    System.out.println("RESULTS = "+results.getRowNumber());
+		  } finally { qexec.close() ; }
 		 
-		 queryString = "PREFIX dmrec: <http://www.recInfo.org/dm/>"
-			 		+ " SELECT ?doc WHERE { "
-			 		+ "?doc rdf:type dmrec:documento"
-			 		+ "?skosQ1 rdf:type skos:concept . "
-			 		+ "?skosQ1 skos:prefLabel ?prefLabel.FILTER regex(?prefLabel,'Edad Media')"
-			 		+ "?skosQ1 skos:narrower+/skos:prefLabel ?labels1"
-			 		+ "?skosQ2 rdf:type skos:concept . "
-			 		+ "?skosQ2 skos:prefLabel ?prefLabel.FILTER regex(?prefLabel,'construccion')"
-			 		+ "?skosQ2 skos:narrower+/skos:prefLabel ?labels2"
-			 		+ "?doc dmrec:keyword ?keyword"
-			 		+ "?keyword skos:prefLabel ?keyLabel"
-			 		+ "FILTER regex(lcase(str(?keyLabel)) = lcase(str(?labels1)) || "
-			 		+ "lcase(str(?keyLabel)) = lcase(str(?keyLabel)))"
+		 System.out.println("----------------------------------------");		
+		 System.out.println("--------------CONSULTA 5----------------");
+		 System.out.println("----------------------------------------");
+		 
+		 queryString = "PREFIX dmrec: <http://www.recInfo.org/dm/> "
+		 			+ "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> "
+		 			+ "PREFIX skos: <http://www.w3.org/2004/02/skos/core#>  "
+		 			+ "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>  "
+			 		+ " SELECT DISTINCT ?doc WHERE { "
+			 		+ "?doc rdf:type ?type . "
+			 		+ "?type rdfs:subClassOf dmrec:document . "
+			 		+ "?skosQ1 rdf:type skos:Concept . "
+			 		+ "?skosQ1 skos:prefLabel ?prefLabel FILTER regex(?prefLabel,'Edad Media','i')"
+			 		+ "?skosQ1 skos:narrower+/skos:prefLabel ?labels1 . "
+			 		+ "?skosQ2 rdf:type skos:Concept . "
+			 		+ "?skosQ2 skos:prefLabel ?prefLabel2 FILTER regex(?prefLabel2,'construccion','i')"
+			 		+ "?skosQ2 skos:narrower+/skos:prefLabel ?labels2 . "
+			 		+ "?doc dmrec:keyword ?keyword . "
+			 		+ "?keyword skos:prefLabel ?keyLabel . "
+			 		+ "FILTER (str(?keyLabel) = str(?labels1) || "
+			 		+ "str(?keyLabel) = str(?labels2))"
 			 		+ "}";
 		
+		  query = QueryFactory.create(queryString) ;
+		   qexec = QueryExecutionFactory.create(query, model) ;
+		  try {
+		    ResultSet results = qexec.execSelect() ;
+		    for ( ; results.hasNext() ; )
+		    {
+		    	QuerySolution soln = results.nextSolution() ;
+		    	Resource z = soln.getResource("doc"); 
+		    	//System.out.println(z.toString());
+		    }
+		    System.out.println("RESULTS = "+results.getRowNumber());
+		  } finally { qexec.close() ; }
+		 
 	}
 	
 }
